3b12d2100174e5b6c97f1d779e4d5c3edf8197ae
vaibhavDharmik1 <vaibhavdharmik55.com>
2021-12-22 08:41:50 -1100

    Start with 00 and bet  every game

---
+++ GamblerSimulatorUC_1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class GamblerSimulatorUC_1 {
+public static final int INITIAL_BET_STAKE = 100;
+public static final int BET_AMT = 1;
+
+
+	public static void main(String[] args) {
+		
+	}
+
+}
