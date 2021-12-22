92406541bdf7b1d8159522fc6b44ff813b92f0c0
vaibhavDharmik1 <vaibhavdharmik55.com>
2021-12-22 09:31:03 -1100

    Gambler make 1 bet so either win or loose 1

---
+++ GamblerSimulatorUC_2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class GamblerSimulatorUC_2 {
+	static int AMOUNT_OF_STAKE=100;
+	static int BET_AMOUNT=1;
+	static int WON=1;
+	static int LOST=0;
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Starting amount of stake is "+AMOUNT_OF_STAKE);
+		 System.out.println("Amount to bet after every play "+BET_AMOUNT);
+		 int play = (int) Math.floor(Math.random() * 10) % 2;
+		 System.out.println("Gamblers play:"+play);
+		 int cashInHand = AMOUNT_OF_STAKE;
+		 switch(play) {
+		 	case 1:
+		 		 cashInHand = cashInHand + BET_AMOUNT;
+		 		 System.out.println("Total current cash gambler has "+cashInHand);
+		 		 break;
+		 	case 0:
+		 		 cashInHand = cashInHand - BET_AMOUNT;
+		 		 System.out.println("Total current cash gambler has "+cashInHand);
+		 		 break;
+		 	default:
+		 		 System.out.println("default");
+		 }
+		 if(play == 1) {
+			 System.out.println("Player won the first play");
+		 }
+		 else {
+			 System.out.println("Player lost the first play");
+		 }
+
+	}
+
+}
